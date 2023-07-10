863b49bab1df07d20dce70939b470b9dec517264
PurosovMV <mihail.purosov@gmail.com>
2023-07-10 22:18:10 +0300

    corrected class Note

--- model/note.py
+++ model/note.py
@@ -4,15 +4,11 @@ from datetime import datetime
 
 class Note:
     def __init__(self, data, text_note):
-         self.__text_note = text_note  # текст заметок
-         self.__data = data  # дата создания заметки
+        self.__text_note = text_note  # текст заметок
+        self.__data = data  # дата создания заметки
 
-    """Метод, возвращающий заметку в виде строки """
-
-    def __str__(self, text_note):
+    def __str__(self, text_note):  # Метод, возвращающий заметку в виде строки
         return f"{self.__data} {self.__text_note}"
 
-        """Метод для изменения текста заметки"""
-
-    def change_text(self, new_text: str):
+    def change_text(self, new_text: str):  # Метод для изменения текста заметки
         self.__text_note = new_text
